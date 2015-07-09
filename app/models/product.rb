1e4ee71874be5249310cbbe55bc37253b5144568
Steven Yap <stevenyap@futureworkz.com>
2015-07-09 10:16:22 +0700

    Add Product#title_is_shorter_than_description validation with spec

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,12 @@ class Product < ActiveRecord::Base
 
   validates :title, :description, presence: true  
   validates :price, numericality: { greater_than: 0 }
+  validate :title_is_shorter_than_description
+
+  def title_is_shorter_than_description
+    return if title.blank? or description.blank?
+    if title.length > description.length
+      errors.add(:title, 'should be shorter than description')
+    end
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe Product, type: :model do
     it { should validate_presence_of :title }
     it { should validate_presence_of :description }
     it { should validate_numericality_of(:price).is_greater_than(0)}
+
+    it 'should check that title is shorter than description' do
+      product = Product.new(title: 'Longer Title', description: 'Shorter', price: 12.99)
+      product.validate
+      expect(product.errors.messages).to include(title: ["should be shorter than description"])
+    end
   end
   
   context 'Association' do
