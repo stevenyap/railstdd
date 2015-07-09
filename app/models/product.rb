5a95de3747f1a9d026bb8459d367dbee9be37d3c
Steven Yap <stevenyap@futureworkz.com>
2015-07-09 11:27:09 +0700

    Update ProductsController#index to display published products only with spec

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,8 @@ class Product < ActiveRecord::Base
   validates :price, numericality: { greater_than: 0 }
   validate :title_is_shorter_than_description
 
+  scope :published, -> { where(published: true) }
+
   def title_is_shorter_than_description
     return if title.blank? or description.blank?
     if title.length > description.length
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,11 +2,25 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
   describe '#index' do
-    let!(:products) { FactoryGirl.create_list(:product, 2) }    
+    context 'Listing of products' do
+      let!(:products) { create_list(:product, 2) }    
 
-    it 'should have a list of products' do
-      get :index
-      expect(assigns(:products).length).to eq 2
+      it 'should have a list of products' do
+        get :index
+        expect(assigns(:products).length).to eq 2
+      end
+    end
+
+    context 'Display Published Products' do
+      let!(:unpublished_product) { create(:product, published: false) }
+      let!(:published_product)   { create(:product, published: true) }
+      let(:assigned_products)    { assigns(:products) }
+     
+      it 'should only display published products' do
+        get :index
+        expect(assigned_products).to include published_product
+        expect(assigned_products).not_to include unpublished_product
+      end
     end
   end
 end
